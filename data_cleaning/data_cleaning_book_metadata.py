ed5a2113709c9210b8b74353a0694b8118c7e6ad
jbjill <89559319+jbjill@users.noreply.github.com>
2021-12-09 17:33:59 -0500

    adding data cleaning files

--- data_cleaning/data_cleaning_book_metadata.py
+++ data_cleaning/data_cleaning_book_metadata.py
@@ -1,30 +1,37 @@
 #adapted from data_cleaning.ipynb
 import gzip
 import json
+from langdetect import detect
+import metapy
+import pandas as pd
 
 
 if __name__=="__main__":
     
-    input_filename="goodreads_books.json.gz"
-    output_filename="goodreads_books_cleaned2.json.gz"
+    input_filename="data/goodreads_books.json.gz"
+    #output_filename="goodreads_books_cleaned3.json.gz"
+    output_filename="data/goodreads_books_cleaned4.json.gz"
 
 
     c=0
 
     popular_shelves = {}
     author_roles = {}
+    
+    data_lan = []
+    book_titles = []
     with gzip.GzipFile(input_filename, 'r',) as fin:
-        with gzip.open(output_filename, 'w') as f:
-            data_lan = []
-            while c < 2360000:
-                #for line in fin:
-                line = fin.readline()
-                c+=1
-                if c%1000==0:
-                    print(c)
-                info=json.loads(line.decode('utf-8'))
-                try:
-                    if ((info['language_code']=='eng') |(info['country_code'] == 'US') ):
+        #with gzip.open(output_filename, 'w') as f:
+        while c < 2360000:
+            #for line in fin:
+            line = fin.readline()
+            c+=1
+            if c%1000==0:
+                print(c)
+            info=json.loads(line.decode('utf-8'))
+            try:
+                if ((info['language_code']=='eng') |(info['country_code'] == 'US') ):
+                    if detect(info['title'])=='en':
                         #print(info)
                         new_info={}
                         new_info['title']=info['title']
@@ -40,9 +47,9 @@ if __name__=="__main__":
                         new_info['text_reviews_count']=info['text_reviews_count']
                         new_info['num_pages'] = info['num_pages']
                         #print((json.dumps(new_info)+'\n').encode('utf-8'))
-                        #data_lan.append(new_info)
-
-                        f.write((json.dumps(new_info)+'\n').encode('utf-8'))
+                        data_lan.append(new_info)
+                        book_titles.append(new_info['title'])
+                        
                         if(new_info['authors']):
                             for author_item in new_info['authors']:
                                 if author_item['role'] in author_roles:
@@ -59,10 +66,35 @@ if __name__=="__main__":
                                 else:
                                     #print('shelf %s added!'%shelf_item['name'])
                                     popular_shelves[shelf_item['name']] = 1
-                except:
-                    pass
-                
+            except:
+                pass
+            
 
+
+    df = pd.DataFrame(book_titles,columns=['book_title'])
+    first_df = df.groupby('book_title').agg(first_index=('index',min))
+    
+    df = df.merge(first_df,how='left',on='book_title')
+    df['unique'] = df['index'] == df['first_index']
+    
+    unique_booktitle = df['unique'].to_numpy()
+
+    with gzip.open(output_filename, 'w') as f:
+        content=[]
+        for i in range(len(data_lan)):
+            data = data_lan[i]
+            if unique_booktitle[i]:
+                content.append((json.dumps(data)+'\n').encode('utf-8'))
+        f.writelines(content)
+        
+    
+    with gzip.open(output_filename, 'w') as f:
+        content=[]
+        for data in data_lan:
+            content.append((json.dumps(data)+'\n').encode('utf-8'))
+        f.writelines(content)
+        
+    
     print(author_roles)
     print(popular_shelves)
 
@@ -75,9 +107,3 @@ if __name__=="__main__":
         for shelf in popular_shelves:
             f.write(shelf)
             f.write('/n')
-
-    #with gzip.open(output_filename, 'w') as f:
-    #    content=[]
-    #    for dict in data_lan:
-    #        content.append((json.dumps(dict)+'\n').encode('utf-8'))
-    #    f.writelines(content)
